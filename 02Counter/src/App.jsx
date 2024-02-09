6cd30e14cfd48eb93a59bcc22f3bb38b4cbac9ce
AnshMehta24 <ansh.mehta.y24@gmail.com>
2024-02-09 14:23:19 +0530

    Basic Understanding and Use Hooks

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+ const [counter, setCounter] =useState(10)
+
+  // let counter = 10;
+  const addValue = ()=>{
+    console.log("Clicked");
+    if(counter === 20){
+      alert("Maximum value is 20")
+    }else{
+      setCounter(counter + 1)
+    }
+  }
+
+  const removeValue = ()=>{
+    if(counter < 1){
+      alert("Keep the Value atleast Minimum to 0")
+    }else{
+      setCounter(counter -1)
+    }
+  }
+
+  return (
+   <>
+   <h1>Code with Ansh</h1>
+   <h2>Ciunter Value : {counter}</h2>
+
+   <button onClick={addValue}
+   >Add value {counter}</button>
+   <br />
+   <button onClick={removeValue}>Remove Value {counter}</button>
+   </>
+  )
+}
+
+export default App
