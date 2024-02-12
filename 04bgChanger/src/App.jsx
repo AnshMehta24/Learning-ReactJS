8762efa19a3e73d82a46107abbf98c43ce469645
AnshMehta24 <ansh.mehta.y24@gmail.com>
2024-02-12 10:16:54 +0530

    commit

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,25 @@
+import {useState} from "react"
+
+function App() {
+  const [color, setColor] = useState("olive")
+
+  return (
+   
+   <div className="w-full h-screen duration-200" style ={{backgroundColor : color}}>
+    <div className="fixed flex flex-wrap justify-center bottom-12  inset-x-0 px-2">
+      <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl">
+        <button className="outline-none px-4   py-1 rounded-full text-white shadow-sm" style={{backgroundColor : "red"}} onClick={()=>{setColor("red")}}>Red</button>
+        <button className="outline-none px-4   py-1 rounded-full text-white shadow-sm" style={{backgroundColor : "green"}}onClick={()=>{setColor("green")}}>Green</button>
+        <button className="outline-none px-4   py-1 rounded-full text-white shadow-sm" style={{backgroundColor : "blue"}}onClick={()=>{setColor("blue")}}>Blue</button>
+        <button className="outline-none px-4   py-1 rounded-full text-white shadow-sm" style={{backgroundColor : "aqua"}}onClick={()=>{setColor("aqua")}}>Aqua</button>
+        <button className="outline-none px-4   py-1 rounded-full text-white shadow-sm" style={{backgroundColor : "black"}}onClick={()=>{setColor("black")}}>Black</button>
+        <button className="outline-none px-4   py-1 rounded-full text-white shadow-sm" style={{backgroundColor : "darkmagenta"}}onClick={()=>{setColor("darkmagenta")}}>darkmagenta</button>
+        <button className="outline-none px-4   py-1 rounded-full text-white shadow-sm" style={{backgroundColor : "yellow"}}onClick={()=>{setColor("yellow")}}>Yellow</button>
+      </div>
+    </div>
+     </div>
+   
+  )
+}
+
+export default App
