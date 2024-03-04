8472bf141ef12267ff8ded0999863df3abc82c21
AnshMehta24 <ansh.mehta.y24@gmail.com>
2024-03-04 21:12:36 +0530

    Commit

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import UserContextProvider from './context/userContextProvider'
+import Login from './component/Login'
+import Profile from './component/Profile'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <UserContextProvider>
+      <h1>Ansh OP</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
---
+++ 08miniContext/src/context/UserContext.js
@@ -0,0 +1,16 @@
+import React, { useCallback } from "react";
+
+const UserContext=React.createContext();
+
+export default UserContext;
+
+
+//Provider
+{/* <UserContext>
+    <Login />
+    <Card>
+        <Data/>
+    </Card>
+
+</UserContext> */}
+
