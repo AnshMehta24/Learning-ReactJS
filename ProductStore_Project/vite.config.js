3cf7527eec27dd5289197a1ec77c1cf9c84f604b
AnshMehta24 <ansh.mehta.y24@gmail.com>
2024-08-11 10:40:34 +0530

    commit

--- ProductStore_Project/vite.config.js
+++ ProductStore_Project/vite.config.js
@@ -1,8 +1,18 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   publicDir: 'public',
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',
+  build: {
+    outDir: 'dist', // Ensure this matches where you're deploying from
+  },
 })
